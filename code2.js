9ffe6d099f57b09fa3b1f860b5867b81145efd38
Bettina Kron <bettinakron@hotmail.com>
2022-05-24 17:23:04 +0200

    Page "resetips" with blogg (grid) and JS function for blog dates

---
+++ code2.js
@@ -0,0 +1,19 @@
+// Händelsehanterare
+window.onload = bloggDate;
+
+// Visa datum för blogghändelser & resetips
+function bloggDate() {
+    
+    let dayName = new Array("söndag", "måndag", "tisdag", "onsdag", "torsdag", "fredag","lördag");
+    let monthName = new Array("janurari", "februari", "mars", "april", "maj", "juni", "juli", "augusti", "september", "october", "november", "december");
+
+    var now = new Date();
+    var dateString = dayName[now.getDay()] + " " + now.getDate() + " " + monthName[now.getMonth()] + " 2022";
+
+    // document.getElementById("blogg1"). innerHTML = "Postad: " + dateString;
+    let dates = document.getElementsByClassName("blogg-date");
+
+    for (let i = 0; i < dates.length; i++) {
+        dates[i].innerHTML = "Postad: " + dateString;
+    }
+}
